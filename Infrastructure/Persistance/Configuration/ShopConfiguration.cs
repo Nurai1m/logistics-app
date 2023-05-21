83d29e58fbe9636efc53e459c2734a6bb3abbfb5
n.iskakova <n.iskakova@it.salyk.kg>
2023-05-21 23:09:35 +0600

    add configurations

---
+++ Infrastructure/Persistance/Configuration/ShopConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class ShopConfiguration : IEntityTypeConfiguration<Shop>
+    {
+        public void Configure(EntityTypeBuilder<Shop> builder)
+        {
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(500);
+            builder.Property(x => x.Description).IsRequired().HasMaxLength(1000);
+        }
+    }
+}
---
+++ Infrastructure/Persistance/Configuration/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistance.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Firstname).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.Patronymic).HasMaxLength(200);
+            builder.Property(x => x.Address).IsRequired().HasMaxLength(1000);
+            builder.Property(x => x.DateOfBirth).IsRequired();
+        }
+    }
+}
